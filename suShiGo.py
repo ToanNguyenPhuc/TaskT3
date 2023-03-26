da9861639bc125c77a65aa9f44223b883f5793f7
NguyenToan <112460683+Togban@users.noreply.github.com>
2023-03-26 12:13:58 +0700

    Add files via upload

--- suShiGo.py
+++ suShiGo.py
@@ -43,16 +43,15 @@ def bot_lv0(state, perData):
     idx = np.random.randint(0, arr_action.shape[0])
     return arr_action[idx], perData
 #----------------------------------------------------------------
-def strategy5(state,per):
+def MainAgent(state,per):
     actions = getValidActions(state)
-    actions = np.where(actions == 1)[0]
+    actions = np.where(actions ==1)[0]
     index_1 = 14*(0 + 1)
     cards_on_hand = state[index_1+2:index_1+14]
     turn = state[1:2] % 7
     # reset per
     if turn == 1:
         per = np.array([5,13,8,0,3,10,4,11,1,15,6,2,12,9])
-
     #--------------#--------------#--------------#--------------#--------------
     if  cards_on_hand[1] == 3:
         per[1] = 0
@@ -62,9 +61,34 @@ def strategy5(state,per):
         per[9] = 0
     if turn > 4 and cards_on_hand[1] < 1:
         per[1] = 0
-    actions_point = per[actions]
-    return actions[np.argmax(actions_point)],per
-
-per = np.array([5,13,8,0,3,10,4,11,1,15,6,2,12,9])
-win1, per = numba_main_2(strategy5, 100000,per , 1)
-print('win',win1)
+    if 9 in actions:
+      return 9,per
+    elif 1 in actions:
+      return 1,per
+    elif 12 in actions:
+      return 12,per
+    elif 7 in actions:
+      return 7,per
+    elif 5 in actions:
+      return 5,per
+    elif 13 in actions:
+      return 13,per
+    elif 2 in actions:
+      return 2,per
+    elif 10 in actions:
+      return 10,per
+    elif 0 in actions:
+      return 0,per
+    elif 6 in actions:
+      return 6,per
+    elif 4 in actions:
+      return 4,per 
+    elif 11 in actions:
+      return 11,per
+    elif 8 in actions:
+      return 8,per
+    elif 3 in actions:
+      return 3,per
+    return np.random.choice(actions),per
+win,per = numba_main_2(MainAgent,10000,np.array([0]),1)
+print('win',win)
